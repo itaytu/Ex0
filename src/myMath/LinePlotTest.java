95df9a64c168c0a24b303cfef24701d0354a1538
itayt <itayt@users.noreply.github.com>
2018-11-11 17:18:27 +0200

    Polynom-Graph-Calculator

---
+++ src/myMath/LinePlotTest.java
@@ -0,0 +1,45 @@
+package myMath;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+import de.erichseifert.gral.data.DataTable;
+import de.erichseifert.gral.plots.XYPlot;
+import de.erichseifert.gral.plots.lines.DefaultLineRenderer2D;
+import de.erichseifert.gral.plots.lines.LineRenderer;
+import de.erichseifert.gral.plots.points.PointRenderer;
+import de.erichseifert.gral.ui.InteractivePanel;
+
+public class LinePlotTest extends JFrame {
+    public LinePlotTest() {
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(600, 400);
+
+        DataTable data = new DataTable(Double.class, Double.class);
+        DataTable data2 = new DataTable(Double.class, Double.class);
+        Polynom p = new Polynom ("0.2x^4-1.5x^3+3.0x^2-x-5");
+        Polynom pd = new Polynom (p.derivative());
+        for (double x = -2; x <= 6; x+=0.25) {
+        	if (pd.f(x)>0 && pd.f(x)<0.5 ) {
+        		double y = p.f(x);
+        		System.out.println("the x axis is: " + x  +" the y axis is: "+ y);
+                data2.add(x, y);
+        	}else {
+        		double y = p.f(x);
+                data.add(x, y);
+        	}
+        }
+        XYPlot plot = new XYPlot(data,data2);
+        getContentPane().add(new InteractivePanel(plot));
+        LineRenderer lines = new DefaultLineRenderer2D();
+        plot.setLineRenderers(data, lines);
+        Color color = new Color(0.0f, 0.3f, 1.0f);
+        plot.getPointRenderers(data).get(0).setColor(color);
+        plot.getPointRenderers(data2).get(0).setColor(color.RED);
+        plot.getLineRenderers(data).get(0).setColor(color);
+    }
+
+    public static void main(String[] args) {
+        LinePlotTest frame = new LinePlotTest();
+        frame.setVisible(true);
+    }
+}
--- src/myMath/MonomTest.java
+++ src/myMath/MonomTest.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package myMath;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -52,59 +51,4 @@ class MonomTest {
 		double x=m1.f(2);
 		assertEquals(58.4, x, "Function is wrong");
 	}
-=======
-package myMath;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-class MonomTest {
-	Monom m1, m2;
-	
-	@BeforeEach
-	void setUp() throws Exception {
-		m1= new Monom(7.3, 3);
-		m2= new Monom(-13, 3);
-	}
-
-	@Test
-	void testDerivative() {
-		Monom m=new Monom(21.9, 2);
-		boolean flag = false;
-		m1=m1.derivative();
-		if(m1.get_coefficient()==m.get_coefficient() && m1.get_power()==m.get_power()) flag = true;
-		assertEquals( true, flag, "Function is wrong");
-	}
-
-	@Test
-	void testAdd() {
-		Monom m=new Monom(-5.7,3);
-		boolean flag = false;
-		m1.add(m2);
-		if(m1.get_coefficient()==m.get_coefficient() && m1.get_power()==m.get_power()) flag = true;
-		assertEquals(true , flag,"Function is wrong");
-	}
-
-	@Test
-	void testMultiply() {
-		Monom m=new Monom(-94.9, 6);
-		boolean flag = false;
-		m1.multiply(m2);
-		assertEquals(m1.get_coefficient(), m.get_coefficient(), 0.0001, "Function is wrong");
-		assertEquals(m1.get_power(), m.get_power(), "Function is wrong");
-	}
-
-	@Test
-	void testEqualsMonom() {
-		assertEquals(false, m1.equals(m2), "Function is wrong");
-	}
-
-	@Test
-	void testF() {
-		double x=2;
-		assertEquals(58.4, m1.f(x), "Function is wrong");
-	}
->>>>>>> 50ae85a741bcbfda6b2a3c4d7f94a43fa4c32fd4
 }
